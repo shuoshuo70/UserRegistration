30543fd8da2a7ac33c56fe901e6b137dbb2d1217
shuoshuo70 <724722392@qq.com>
2017-06-05 12:37:12 +0800

    control anction with spring

--- test/com/registration/action/UserActionTest.java
+++ test/com/registration/action/UserActionTest.java
@@ -1,18 +1,34 @@
 package com.registration.action;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.registration.vo.UserRegistorInfo;
 
 public class UserActionTest {
 
 	@Test
 	public void testExecute() throws Exception{
 		UserAction ua = new UserAction();
-		ua.setUsername("e");
-		ua.setPassword("e");
+		UserRegistorInfo info = new UserRegistorInfo();
+		info.setUsername("e");
+		info.setPassword("e");
+		info.setPassword2("e");
+		ua.setInfo(info);
 		String ret = ua.execute();
 		assertEquals("sucess", ret);
+	}
+	
+	@Test
+	public void testList() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		UserAction ua = (UserAction) ctx.getBean("user");
+		ua.list();
+		assertTrue(ua.getUsers().size() > 0);
 	}
 
 }
